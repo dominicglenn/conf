14f94339d29d4f2fa9d3ccfebebd56317c4943f1
Dominic Santos <dominicglenn@gmail.com>
2016-08-22 11:56:15 +0100

    Update .phoenix.js

--- .phoenix.js
+++ .phoenix.js
@@ -6,18 +6,54 @@ Key.on('f', [ 'cmd', 'alt' ], function () {
   }
 });
 
-Key.on('c', [ 'cmd', 'alt' ], function () {
+
+function resizeAndCentre(widthRatio, heightRatio){
   var focused = Window.focused();
 
   if(focused){
-    var size = focused.frame();
+    var size;
     var screen = Screen.main().frame();
+
+    size = {
+      width: screen.width * widthRatio,
+      height: screen.height * heightRatio,
+    };
 
     focused.setTopLeft({
       x: screen.width / 2 - size.width / 2,
       y: screen.height / 2 - size.height / 2
     });
+
+    focused.setSize(size);
   }
+}
+
+Key.on('m', [ 'cmd', 'alt' ], function(){
+  var widthRatio = 0.5;
+  var heightRatio = 0.48;
+
+  resizeAndCentre(widthRatio, heightRatio);
+});
+
+Key.on('c', [ 'cmd', 'alt' ], function(){
+  var widthRatio = 0.55;
+  var heightRatio = 0.6;
+
+  resizeAndCentre(widthRatio, heightRatio);
+});
+
+Key.on('k', [ 'cmd', 'alt' ], function(){
+  var widthRatio = 0.6;
+  var heightRatio = 0.65;
+
+  resizeAndCentre(widthRatio, heightRatio);
+});
+
+Key.on('l', [ 'cmd', 'alt' ], function () {
+  var widthRatio = 0.70;
+  var heightRatio = 0.65;
+
+  resizeAndCentre(widthRatio, heightRatio);
 });
 
 Key.on('j', [ 'cmd', 'shift' ], function () {
